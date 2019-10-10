ec4b21519e3b4f61f0c17e411f91fedb6d08bdb2
MRITYUNJAY <mrityunjay.1@mountblue.io>
2019-10-11 03:56:24 +0530

    Adds homepage along with login/signup features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
+import Homepage from "./components/auth/homepage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Twitter</h1>
-      </header>
+    <Router>
+      <Route path="/" component={Homepage} />
+      </Router>
     </div>
   );
 }
